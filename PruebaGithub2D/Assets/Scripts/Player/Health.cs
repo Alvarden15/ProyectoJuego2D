9a97cc9b88a98a49aa7d0e8dd3bb02fcd1b13161
Alvarden <alvaroracchumi@hotmail.com>
2019-08-27 11:37:36 -0500

    Reestructuración parcial del nivel
    Hice algunos ajustes a las plataformas y nuevos elementos fueron agregados. Esto quizas le de más vida al nivel.

---
+++ PruebaGithub2D/Assets/Scripts/Player/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    Slider health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health=gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
